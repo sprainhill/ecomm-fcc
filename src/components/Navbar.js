1bbea7d26d1e72e15259b4fb285658de3958eb65
Rory Murray <rory.christian.murray@gmail.com>
2020-01-22 17:11:23 -0500

    Close Navbar ticket

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import ButtonContainer from './Button'
+import styled from 'styled-components'
 
-const Navbar = () => {
+export default () => {
     return (
-        <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
             <Link to="/" >
             <i class="fas fa-book" style={{color: "fuchsia"}}></i>
             </Link>
             <ul className="navbar-nav align-items-center">
                 <li className="nav-item ml-5">
                     <Link to="/" className="nav-link" >
-                    Products
+                    products
                     </Link>
                 </li>
             </ul>
@@ -23,14 +24,17 @@ const Navbar = () => {
                     my cart
                 </ButtonContainer>
             </Link>
-        </nav>
+        </NavWrapper>
 
     );
 };
 
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link {
+    color: var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform: capitalize;
+}
 
-
-
-
-
-export default Navbar;
+`
