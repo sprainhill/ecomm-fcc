0a82327e668513c0069a47392932fa7582646152
Rory Murray <rory.christian.murray@gmail.com>
2020-01-22 15:51:38 -0500

    Setup Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import logo from "./logo.svg"
 
 const Navbar = () => {
     return (
-        <div>
-            <div>navbar</div>
-        </div>
+        <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <Link to="/" >
+            <i class="fas fa-book" style={{color: "fuchsia"}}></i>
+            </Link>
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link" >
+                    Products
+                    </Link>
+                </li>
+            </ul>
+             <Link to="/cart" className="ml=auto" >
+                <button>
+                    <i className="fas fa fa-cart-plus" />
+                    my cart
+                </button>
+            </Link>
+        </nav>
+
     );
 };
 
