714d4f1ce6f81a44cd61bf9573d00358ca1d6b3a
Rory Murray <rory.christian.murray@gmail.com>
2020-01-22 13:56:08 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="container">
-      <div className="row" >
-        <div className="col-6">
-          <span><i className="fas fa-home" /></span>
-        </div>
-        <div className="col-6">
-        Col II
-        </div>
-      </div>
-    </div>
+    <>
+    <h3>
+      Marketplace
+    </h3>
+    </>
   );
 }
 
