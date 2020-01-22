20bb9dcbfea142fda8919796fe8aab74e6086f61
Rory Murray <rory.christian.murray@gmail.com>
2020-01-22 12:58:36 -0500

    Add and test fontawesome'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello, world</div>
+    <div className="container">
+      <div className="row" >
+        <div className="col-6">
+          <span><i className="fas fa-home" /></span>
+        </div>
+        <div className="col-6">
+        Col II
+        </div>
+      </div>
     </div>
   );
 }
