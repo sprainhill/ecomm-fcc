7b34bd963b3cd32e11a26ab6d7a739b688f6ae73
Rory Murray <rory.christian.murray@gmail.com>
2020-01-22 16:49:12 -0500

    Fix spacing typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import logo from "./logo.svg"
-import styled from "styled-components"
+import ButtonContainer from './Button'
 
 const Navbar = () => {
     return (
@@ -16,7 +15,7 @@ const Navbar = () => {
                     </Link>
                 </li>
             </ul>
-             <Link to="/cart" className="ml=auto" >
+             <Link to="/cart" className="ml-auto" >
                 <ButtonContainer>
                     <span className="mr-2" />
                     <i className="fas fa fa-cart-plus" />
@@ -29,25 +28,6 @@ const Navbar = () => {
     );
 };
 
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size:1.4rem;
-background:transparent;
-border:0.075rem solid var(--lightBlue);
-color: var(--lightBlue);
-border-radius: 0.5rem;
-padding: 0.2rem 0.5rem;
-cursor: pointer;
-margin: 0.5rem 0.5rem 0.2rem 0;
-transition: all 0.5s ease-in-out;
-&:hover{
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-}
-&:focus {
-    outline: none;
-}
-`
 
 
 
