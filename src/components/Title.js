fbd530b6b8c0e61ba23e559dfbf3df8b21b0514e
Rory Murray <rory.christian.murray@gmail.com>
2020-01-22 17:14:40 -0500

    Instantiate Title component

---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div>
+            <div>
+                Title
+            </div>
+            
+        </div>
+    );
+};
+
+export default Title;
+<div>
+    Title
+</div>
