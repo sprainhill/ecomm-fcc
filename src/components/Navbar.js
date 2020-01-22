34461df9f2f2b7fedfeaf97b6f98aaf88033481f
Rory Murray <rory.christian.murray@gmail.com>
2020-01-22 16:34:17 -0500

    Style button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import logo from "./logo.svg"
+import styled from "styled-components"
 
 const Navbar = () => {
     return (
@@ -16,14 +17,40 @@ const Navbar = () => {
                 </li>
             </ul>
              <Link to="/cart" className="ml=auto" >
-                <button>
+                <ButtonContainer>
+                    <span className="mr-2" />
                     <i className="fas fa fa-cart-plus" />
+                    <span className="ml-2" />
                     my cart
-                </button>
+                </ButtonContainer>
             </Link>
         </nav>
 
     );
 };
+
+const ButtonContainer = styled.button`
+text-transform:capitalize;
+font-size:1.4rem;
+background:transparent;
+border:0.075rem solid var(--lightBlue);
+color: var(--lightBlue);
+border-radius: 0.5rem;
+padding: 0.2rem 0.5rem;
+cursor: pointer;
+margin: 0.5rem 0.5rem 0.2rem 0;
+transition: all 0.5s ease-in-out;
+&:hover{
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+}
+&:focus {
+    outline: none;
+}
+`
+
+
+
+
 
 export default Navbar;
