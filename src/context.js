c4dcbce457b1f81acbf14a5d4eb85cb10bfcaa10
unknown <rory.christian.murray@gmail.com>
2020-01-26 17:18:05 -0500

    Import data and set to state

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {storeProducts, detailProduct} from "./data"
 
 const ProductContext = React.createContext()
 
@@ -7,10 +8,16 @@ const ProductContext = React.createContext()
 // Consumer
 
 class ProductProvider extends React.Component {
+    state = {
+        products: storeProducts,
+        detailProduct
+    }
+
+
 
     render() {
         return (
-            <ProductContext.Provider value="hello from context">
+            <ProductContext.Provider value={{}}>
                 {this.props.children}
             </ProductContext.Provider>
         )
