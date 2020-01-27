124add7dd45d5609cb0a67f0a614dcf3f6fa0d76
unknown <rory.christian.murray@gmail.com>
2020-01-27 01:13:13 -0500

    Finish ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import Product from "./Product"
 import Title from "./Title"
-import { storeProducts } from "../data"
 import { ProductConsumer } from "../context"
 
 const ProductList = () => {
-    const [products, setProducts] = React.useState([...storeProducts])
-    console.log("products : ", products)
-
 
     return (
         <>
@@ -18,7 +14,9 @@ const ProductList = () => {
             <div className="row ml-5" >
                 <ProductConsumer>
                     {value=> {
-                    return <div>{value}</div>
+                    return value.products.map(product => {
+                        return <Product product={product} key={product.id} /> 
+                    })
 
                     }}
                 </ProductConsumer>
