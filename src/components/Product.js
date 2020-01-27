d43696197409ca8ee1024c3b4a771959cfc6f7bf
unknown <rory.christian.murray@gmail.com>
2020-01-27 01:26:24 -0500

    Setup bootstrap for Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import styled from "styled-components"
+import { Link } from "react-router-dom"
 
-const Product = () => {
+export default props => {
+
+    const {id, title, img, price, inCart} = props.product
+
     return (
-        <div>
+        <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
             <div>Product</div>
-            
-        </div>
+        </ProductWrapper>
     );
 };
 
-export default Product;
+
+const ProductWrapper = styled.div`
+
+
+`
+
+
