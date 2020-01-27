6f04da6709be89d1445507b7365786321422614a
unknown <rory.christian.murray@gmail.com>
2020-01-27 12:12:24 -0500

    Create add to cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,6 +13,7 @@ export default props => {
                 <Link to='/details'>
                     <img src={img} alt="product" className="card-img-top" />                </Link>
                 </div>
+    <button className="cart-btn" disabled={inCart ? true : false} onClick={()=>{console.log("added to cart")}}>{inCart ? (<p className="text-capitalize mb-0" >in cart</p>) : (<i className="fas fa-cart-plus " />)}</button>
             </div>
         </ProductWrapper>
     );
