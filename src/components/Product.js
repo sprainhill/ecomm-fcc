b99dd4ff6a0f0f9c3ab0ec17cdf650a5b2900107
unknown <rory.christian.murray@gmail.com>
2020-01-28 18:34:01 -0500

    Footer init

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,15 +7,21 @@ export default props => {
     const {id, title, img, price, inCart} = props.product
 
     return (
-        <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
-            <div className="card">
+        <div className="card">
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
                 <div className="img-container p-5" onClick={console.log("clicked")}>
                 <Link to='/details'>
                     <img src={img} alt="product" className="card-img-top" />                </Link>
                 </div>
     <button className="cart-btn" disabled={inCart ? true : false} onClick={()=>{console.log("added to cart")}}>{inCart ? (<p className="text-capitalize mb-0" >in cart</p>) : (<i className="fas fa-cart-plus " />)}</button>
-            </div>
+        
+        <div className="card-wrapper d-flex justify-content-between">
+            <p className="align-self-center">Footer</p>
+        </div>
         </ProductWrapper>
+
+        
+            </div>
     );
 };
 
