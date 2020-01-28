42128d52409f43482b5ec61c1e9cafaf104b6316
Rory Murray <rory.christian.murray@gmail.com>
2020-01-28 18:54:19 -0500

    Add footer

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,21 +7,21 @@ export default props => {
     const {id, title, img, price, inCart} = props.product
 
     return (
-        <div className="card">
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
-                <div className="img-container p-5" onClick={console.log("clicked")}>
-                <Link to='/details'>
-                    <img src={img} alt="product" className="card-img-top" />                </Link>
-                </div>
+    <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
+    <div className="card">
+    <div className="img-container p-5" onClick={() => console.log("clicked")}>
+        <Link to='/details'>
+            <img src={img} alt="product" className="card-img-top" />                </Link>
     <button className="cart-btn" disabled={inCart ? true : false} onClick={()=>{console.log("added to cart")}}>{inCart ? (<p className="text-capitalize mb-0" >in cart</p>) : (<i className="fas fa-cart-plus " />)}</button>
-        
-        <div className="card-wrapper d-flex justify-content-between">
-            <p className="align-self-center">Footer</p>
-        </div>
+    </div>
+    { /* footer  */} 
+    <div className="card-footer d-flex justify-content-between" >
+        <p className="align-self-center mb-0" > {title}</p>
+<h5 className="text-blue font-italic mb-0"> <span className="mr-1">$</span>{price}</h5>
+   
+         </div>  
+    </div>
         </ProductWrapper>
-
-        
-            </div>
     );
 };
 
