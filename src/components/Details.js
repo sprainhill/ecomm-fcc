760ecc8942a5b0d86b321e4be14d2b9d59cfc258
Rory Murray <rory.christian.murray@gmail.com>
2020-01-28 21:27:56 -0500

    Setup title in details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { ProductConsumer } from '../context'
+import { Link } from 'react-router-dom'
+import ButtonContainer from './Button'
 
 const Details = () => {
     return (
-        <div>
-            <div>Details</div>
-        </div>
+        <ProductConsumer>
+            {value => {
+                const {id, company, title, price, img, info, inCart} = value.detailProduct
+                return (
+                    <div className="container py-5">
+                        <div className="row">
+                            <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                <h1>{title}</h1>
+
+                            </div>
+                        </div>
+                    </div>
+                )
+            
+            }}
+        </ProductConsumer>
     );
 };
 
