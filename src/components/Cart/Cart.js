fd3d5b2ff7b92f224f4dd12ddce1e76f9331bb73
Rory Murray <rory.christian.murray@gmail.com>
2020-01-29 18:14:27 -0500

    Work on conditional rendering for empty cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import Title from "../Title"
 import CartColumns from "./CartColumns"
+import EmptyCart from "./EmptyCart"
+import { ProductConsumer } from "../../context"
+
 
 const Cart = () => {
     return (
         <section>
-            <Title name="your" title="cart" />
-            <CartColumns />
+        <ProductConsumer>
+            {value => {
+                const { cart } = value;
+                if (cart.length > 0) {
+                    return (
+                        <>
+                    <Title name="your" title="cart" />
+                    <CartColumns />
+                        </>
+                    )
+                } else {
+                    return <EmptyCart />;
+                }
+            }}
+        </ProductConsumer>
+
+        
         </section>
     );
 };
