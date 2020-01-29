ff08424fd7b0d11702f6fa5035660dc4c9229df8
Rory Murray <rory.christian.murray@gmail.com>
2020-01-29 17:34:59 -0500

    Init CartColumns

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from "../Title"
+import CartColumns from "./CartColumns"
 
 const Cart = () => {
     return (
         <section>
             <Title name="your" title="cart" />
+            <CartColumns />
         </section>
     );
 };
---
+++ src/components/Cart/CartColumns.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartColumns = () => {
+    return (
+        <div>
+            <div>Cart columns</div>
+            
+        </div>
+    );
+};
+
+export default CartColumns;
