f5c1a431ef30ce38937a06c639345938c31cc413
Rory Murray <rory.christian.murray@gmail.com>
2020-01-29 17:16:21 -0500

    Edit main to Cart.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from "./components/Navbar"
 import ProductList from "./components/ProductList"
-import Cart from "./components/Cart"
 import Details from "./components/Details"
 import Default from "./components/Default"
 import Modal from "./components/Modal"
+import Cart from "./components/Cart"
 
 function App() {
   console.log("in App")
