abb9024af2316b8d3d98f13398becf38fa95c182
Rory Murray <rory.christian.murray@gmail.com>
2020-01-28 19:33:14 -0500

    Add image transitions

--- src/components/Product.js
+++ src/components/Product.js
@@ -27,6 +27,48 @@ export default props => {
 
 
 const ProductWrapper = styled.div`
+.card {
+    border-color: transparent;
+    transition: all 1s linear;
+}
+
+.card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+}
+&:hover {
+    .card {
+        border: 0.04rem solid rgba(0,0,0,0.2);
+        box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .card-footer {
+        background:rgba(247,247,247);
+    }
+}
+
+.img-container {
+    position: relative;
+    overflow: hidden;
+}
+
+.card-img-top {
+    transform: all 1s linear;
+
+}
+
+.img-container:hover .card-img-top {
+    transform: scale(1.2);
+
+}
+
+.cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+}
+
+
 
 
 `
