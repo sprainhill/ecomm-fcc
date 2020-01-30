a47761d0011965a6bbc13d4af301dc994463d219
Rory Murray <rory.christian.murray@gmail.com>
2020-01-30 18:25:31 -0500

    Add custom url error handling

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Default = () => {
+const Default = props => {
+    console.log("Default props : ", props)
     return (
-        <div>
-            <div>Page Not Found</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+                    <h1>404</h1>
+                    <h1>error</h1>
+                    <h2>page not found</h2>
+    <h3>The requested url <span className="text-danger">{props.location.pathname}</span> was not found</h3>
+                </div>
+            </div>
         </div>
     );
 };
