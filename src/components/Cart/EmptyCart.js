4b80f2a1343be9340d61a0fb907886c1d1f827cc
Rory Murray <rory.christian.murray@gmail.com>
2020-01-30 10:52:44 -0500

    Finish EmptyCart

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -6,9 +6,7 @@ const EmptyCart = () => {
             <div className="row">
                 <div className="col-10 mx-auto text-center text-title">
                     <h1>cart is empty</h1>
-
                 </div>
-
             </div>
             
         </div>
