eb738690b4d305b7672fff0808bc13c5a1c06871
Rory Murray <rory.christian.murray@gmail.com>
2020-01-30 10:57:49 -0500

    Init CartList and CartItem

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartItem = () => {
+    return (
+        <div>
+            <div>Cart item</div>
+        </div>
+    );
+};
+
+export default CartItem;
---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CartItem from "./CartItem"
+
+const CartList = () => {
+    return (
+        <div>
+            <div>Cart list</div>
+        </div>
+    );
+};
+
+export default CartList;
